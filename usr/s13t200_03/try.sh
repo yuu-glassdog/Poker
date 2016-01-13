2fa10fc3495e74e65b6875b1b62b180a7cb2d2d5
Yuu <s13t232@stmail.eng.kagawa-u.ac.jp>
2016-01-13 19:16:53 +0900

    change try number to 10000

--- usr/s13t200_03/try.sh
+++ usr/s13t200_03/try.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 
 sh ../../sys/Make2015.sh PokerOpe.c
-../../sys/DeckInit.exe 1000 > Deck_03.txt
-./PokerExec.exe s13t232 1000 Deck_03.txt 0
+../../sys/DeckInit.exe 10000 > Deck_03.txt
+./PokerExec.exe s13t232 10000 Deck_03.txt 0
